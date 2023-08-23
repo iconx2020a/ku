b4cc96fd3691d08525e1ba8e4414f405fd214d34
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-08-23 20:17:20 +0200

    Update SecretManager.java
    update

--- secretmanagement/src/main/java/secretapp/SecretManager.java
+++ secretmanagement/src/main/java/secretapp/SecretManager.java
@@ -1,40 +1,22 @@
 package secretapp;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;	
-
 public class SecretManager {
-
-    final int size = 3;
-    private String[] conf;
-    private PreparedStatement ps;
-    private ResultSet rs;
-    private static Connection conn;
-    
      String dbPassword = "password";
      String dbUserName = "name";
      String dbURL = "dburl";
     Region region = Region.of("us-east-1");
-
     GetSecretValueResponse getSecretValueResponse;
     SecretsManagerClient client;
      GetSecretValueRequest getSecretValueRequest;
-
     public SecretManager() {
          client = SecretsManagerClient.builder()
             .region(region)
             .build();
-        
           try {
         readFile();
-       
     } catch (Exception e) {
         
         throw e;
@@ -55,16 +37,14 @@ public class SecretManager {
             .build();
              getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
              secret = getSecretValueResponse.secretString();
-            
              System.out.println(secret);
-              
+        
             getSecretValueRequest = GetSecretValueRequest.builder()
             .secretId(dbPassword)
             .build();
              getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
              secret = getSecretValueResponse.secretString();
              System.out.println(secret);
-
     }
     public static void main (String [ ] args){
       new SecretManager();
